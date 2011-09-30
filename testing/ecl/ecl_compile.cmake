0eff04fc4e922a2ed6f97db7b1f3836ff878a775
Richard Chapman <rchapman@hpccsystems.com>
2011-09-30 12:27:28 +0100

    CMake support for ecl
    Add support for debug options and clean up the cmake file a little.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- testing/ecl/ecl_compile.cmake
+++ testing/ecl/ecl_compile.cmake
@@ -3,13 +3,6 @@
 MACRO(COMPILE_WORKUNIT source)
 
 GET_FILENAME_COMPONENT(baseName ${source} NAME_WE)
-#ADD_CUSTOM_TARGET(dependencies_${baseName}
-#    ${CMAKE_COMMAND}
-#    -DTEMPLATE=${CMAKE_SOURCE_DIR}/filelist.cmake.in
-#    -DVARIABLE=DEPENDENCIES
-#    -Dsource=${source}
-#    -DOUTPUT=${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt
-#    -P ${CMAKE_SOURCE_DIR}/ecl_dependencies.cmake)
 
 IF(NOT EXISTS ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
    EXECUTE_PROCESS(
@@ -35,21 +28,18 @@ ADD_CUSTOM_COMMAND(
 #    DEPENDS ${DEPENDENCIES}
 )
 
+IF ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+  SET (USE_ECL_OPTIONS ${ECL_OPTIONS} -g -save-temps ${ECL_OPTIONS_DEBUG})
+ELSE()
+  SET (USE_ECL_OPTIONS ${ECL_OPTIONS} ${ECL_OPTIONS_RELEASE})
+ENDIF()
+
 ADD_CUSTOM_COMMAND(
     OUTPUT ${baseName}
-    COMMAND eclcc -o${baseName} ${source} 
+    COMMAND eclcc -o${baseName} ${USE_ECL_OPTIONS} ${source}
     DEPENDS ${DEPENDENCIES})
-
-#ADD_CUSTOM_TARGET(make_${baseName} DEPENDS ${baseName})
 
 ADD_CUSTOM_TARGET(remake_${baseName} ALL DEPENDS ${baseName} ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
 
-#ADD_CUSTOM_TARGET(remake_${baseName} ALL
-#    COMMAND ${CMAKE_COMMAND}
-#    --build ${CMAKE_BINARY_DIR}
-#    --target dependencies_${baseName}
-#    COMMAND ${CMAKE_COMMAND}
-#    --build ${CMAKE_BINARY_DIR}
-#    --target make_${baseName})
 ENDMACRO()
 
