6d09d0a401f1b83c0b556faf8dfae34a18a04f72
Richard Chapman <rchapman@hpccsystems.com>
2011-09-30 09:16:13 +0100

    Build regression suite via cmake
    Testing the ability to build the regression suite via cmake.
    
    Renamed some files to avoid eclcc weirdness.
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- testing/ecl/ecl_dependencies.cmake
+++ testing/ecl/ecl_dependencies.cmake
@@ -1,6 +1,6 @@
 # dependencies.cmake:
 EXECUTE_PROCESS(
-        COMMAND bash -c "eclcc -E ${source} 2>/dev/null | grep sourcePath= | sed 's/.*sourcePath=\\\"\\([^\\\"]*\\).*/\\1/'"
+        COMMAND bash -c "eclcc -E ${source} 2>/dev/null | grep sourcePath= | grep -v -i lib_ | grep -v -i std[/] | sed 's/.*sourcePath=\\\"\\([^\\\"]*\\).*/\\1/'"
         OUTPUT_VARIABLE FILES
 )
 #SET (FILES hello.ecl world.ecl)
