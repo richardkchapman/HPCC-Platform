03cf7123afb57272aa6626735556d9d6a954d22f
Richard Chapman <rchapman@hpccsystems.com>
2012-01-17 12:58:21 +0000

    Support for deploying regression suite tests when changed
    Use cmake to deploy regression suite tests only when changed
    
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- testing/regress/ecl_compile.cmake
+++ testing/regress/ecl_compile.cmake
@@ -8,7 +8,7 @@ IF(NOT EXISTS ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
   SET (OUTPUT ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
   EXECUTE_PROCESS(
         COMMAND bash -c "echo SET \\\(DEPENDENCIES  >${OUTPUT};
-                         eclcc -E ${source} 2>/dev/null | grep sourcePath= | grep -v -i lib_ | grep -v -i std[/] | sed 's/.*sourcePath=\\\"\\([^\\\"]*\\).*/\\1/' >> ${OUTPUT};
+                         eclcc -I ${CMAKE_CURRENT_SOURCE_DIR} -E ${source} 2>/dev/null | grep sourcePath= | grep -v -i lib_ | grep -v -i std[/] | sed 's/.*sourcePath=\\\"\\([^\\\"]*\\).*/\\1/' >> ${OUTPUT};
                          echo \\\) >>${OUTPUT}"
   )
 ENDIF()
@@ -22,6 +22,7 @@ ADD_CUSTOM_COMMAND(
      -DTEMPLATE=${CMAKE_SOURCE_DIR}/filelist.cmake.in
      -DVARIABLE=DEPENDENCIES
      -Dsource=${source}
+     -Dsourcedir=${CMAKE_CURRENT_SOURCE_DIR}
      -DOUTPUT=${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt
      -P ${CMAKE_SOURCE_DIR}/ecl_dependencies.cmake
     DEPENDS ${DEPENDENCIES}
@@ -33,11 +34,25 @@ ELSE()
   SET (USE_ECL_OPTIONS ${ECL_OPTIONS} -I ${CMAKE_CURRENT_SOURCE_DIR} ${ECL_OPTIONS_RELEASE})
 ENDIF()
 
-ADD_CUSTOM_COMMAND(
+IF (REGRESS_LOCAL)
+  ADD_CUSTOM_COMMAND(
     OUTPUT ${baseName}
     COMMAND eclcc -o${baseName} ${USE_ECL_OPTIONS} ${source}
     DEPENDS ${DEPENDENCIES})
 
-ADD_CUSTOM_TARGET(remake_${baseName} ALL DEPENDS ${baseName} ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+  ADD_CUSTOM_TARGET(remake_${baseName} ALL DEPENDS ${baseName} ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+ELSE()
+  ADD_CUSTOM_COMMAND(
+    OUTPUT ${baseName}.xml
+    COMMAND eclcc -o${baseName}.xml -E ${USE_ECL_OPTIONS} ${source}
+    DEPENDS ${DEPENDENCIES})
+
+  ADD_CUSTOM_COMMAND(
+    OUTPUT deploy_${REGRESS_CLUSTER}_${baseName}.log
+    COMMAND ecl publish ${baseName}.xml --activate --cluster=${REGRESS_CLUSTER} --name=${baseName} > deploy_${REGRESS_CLUSTER}_${baseName}.log
+    DEPENDS ${baseName}.xml ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+
+  ADD_CUSTOM_TARGET(deploy_${REGRESS_CLUSTER}_${baseName} ALL DEPENDS deploy_${REGRESS_CLUSTER}_${baseName}.log)
+ENDIF()
 
 ENDMACRO()
--- testing/regress/ecl_dependencies.cmake
+++ testing/regress/ecl_dependencies.cmake
@@ -1,7 +1,7 @@
 # dependencies.cmake:
 EXECUTE_PROCESS(
-        COMMAND bash -c "echo SET \\\(DEPENDENCIES  >${OUTPUT};
-                         eclcc -E ${source} 2>/dev/null | grep sourcePath= | grep -v -i lib_ | grep -v -i std[/] | sed 's/.*sourcePath=\\\"\\([^\\\"]*\\).*/\\1/' >> ${OUTPUT};
+        COMMAND bash -c "echo set \\\(DEPENDENCIES  >${OUTPUT};
+                         eclcc -I ${sourcedir} -E ${source} 2>/dev/null | grep sourcePath= | grep -v -i lib_ | grep -v -i std[/] | sed 's/.*sourcePath=\\\"\\([^\\\"]*\\).*/\\1/' >> ${OUTPUT};
                          echo \\\) >>${OUTPUT}"
 
 )
