03bb8d0a96ee729d8082b3cd7c44e3527101e15c
Richard Chapman <rchapman@hpccsystems.com>
2012-01-20 11:02:24 +0000

    Clean up CMake files a bit
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- testing/regress/ecl_compile.cmake
+++ testing/regress/ecl_compile.cmake
@@ -4,8 +4,12 @@ MACRO(COMPILE_WORKUNIT source)
 
 GET_FILENAME_COMPONENT(baseName ${source} NAME_WE)
 
-IF(NOT EXISTS ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
-  SET (OUTPUT ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+SET (DEPENDENCIES_DIR "${CMAKE_BINARY_DIR}/dependencies")
+FILE (MAKE_DIRECTORY ${DEPENDENCIES_DIR})
+
+IF(NOT EXISTS ${DEPENDENCIES_DIR}/${baseName}.dep)
+  MESSAGE ("Checking dependencies for ${baseName}")
+  SET (OUTPUT ${DEPENDENCIES_DIR}/${baseName}.dep)
   EXECUTE_PROCESS(
         COMMAND bash -c "echo SET \\\(DEPENDENCIES  >${OUTPUT};
                          eclcc -I ${CMAKE_CURRENT_SOURCE_DIR} -Md ${source} 2>/dev/null >> ${OUTPUT};
@@ -13,17 +17,17 @@ IF(NOT EXISTS ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
   )
 ENDIF()
 
-INCLUDE(${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+INCLUDE(${DEPENDENCIES_DIR}/${baseName}.dep)
 
 ADD_CUSTOM_COMMAND(
-    OUTPUT ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt
+    OUTPUT ${DEPENDENCIES_DIR}/${baseName}.dep
     COMMAND
     ${CMAKE_COMMAND}
      -DTEMPLATE=${CMAKE_SOURCE_DIR}/filelist.cmake.in
      -DVARIABLE=DEPENDENCIES
      -Dsource=${source}
      -Dsourcedir=${CMAKE_CURRENT_SOURCE_DIR}
-     -DOUTPUT=${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt
+     -DOUTPUT=${DEPENDENCIES_DIR}/${baseName}.dep
      -P ${CMAKE_SOURCE_DIR}/ecl_dependencies.cmake
     DEPENDS ${DEPENDENCIES}
 )
@@ -35,24 +39,31 @@ ELSE()
 ENDIF()
 
 IF (REGRESS_LOCAL)
+  SET (BINARY_DIR "${CMAKE_BINARY_DIR}/bin")
+  FILE (MAKE_DIRECTORY ${BINARY_DIR})
   ADD_CUSTOM_COMMAND(
-    OUTPUT ${baseName}
-    COMMAND eclcc -o${baseName} ${USE_ECL_OPTIONS} ${source}
+    OUTPUT ${baseName}.out
+    COMMAND eclcc -o${baseName}.out ${USE_ECL_OPTIONS} ${source}
     DEPENDS ${DEPENDENCIES})
 
-  ADD_CUSTOM_TARGET(remake_${baseName} ALL DEPENDS ${baseName} ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+  ADD_CUSTOM_TARGET(${baseName} ALL DEPENDS ${baseName}.out ${DEPENDENCIES_DIR}/${baseName}.dep)
 ELSE()
+  SET (ARCHIVE_DIR "${CMAKE_BINARY_DIR}/archive")
+  FILE (MAKE_DIRECTORY ${ARCHIVE_DIR})
+  SET (DEPLOY_DIR "${CMAKE_BINARY_DIR}/deploy")
+  FILE (MAKE_DIRECTORY ${DEPLOY_DIR})
+
   ADD_CUSTOM_COMMAND(
-    OUTPUT ${baseName}.xml
-    COMMAND eclcc -o${baseName}.xml -E ${USE_ECL_OPTIONS} ${source}
+    OUTPUT ${ARCHIVE_DIR}/${baseName}.xml
+    COMMAND eclcc -o${ARCHIVE_DIR}/${baseName}.xml -E ${USE_ECL_OPTIONS} ${source}
     DEPENDS ${DEPENDENCIES})
 
   ADD_CUSTOM_COMMAND(
-    OUTPUT deploy_${REGRESS_CLUSTER}_${baseName}.log
-    COMMAND ecl publish ${baseName}.xml --activate --cluster=${REGRESS_CLUSTER} --name=${baseName} > deploy_${REGRESS_CLUSTER}_${baseName}.log
-    DEPENDS ${baseName}.xml ${CMAKE_BINARY_DIR}/dependencies_${baseName}.txt)
+    OUTPUT ${DEPLOY_DIR}/${baseName}.log
+    COMMAND ecl publish ${ARCHIVE_DIR}/${baseName}.xml --activate --cluster=${REGRESS_CLUSTER} --name=${baseName} > ${DEPLOY_DIR}/${baseName}.log
+    DEPENDS ${ARCHIVE_DIR}/${baseName}.xml ${DEPENDENCIES_DIR}/${baseName}.dep)
 
-  ADD_CUSTOM_TARGET(deploy_${REGRESS_CLUSTER}_${baseName} ALL DEPENDS deploy_${REGRESS_CLUSTER}_${baseName}.log)
+  ADD_CUSTOM_TARGET(${baseName} ALL DEPENDS ${DEPLOY_DIR}/${baseName}.log)
 ENDIF()
 
 ENDMACRO()
