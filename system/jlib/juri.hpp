c939fac1965a985793a2fa64dbc63477c6afa207
Renato Golin <rengolin@systemcall.org>
2012-07-04 15:27:33 +0100

    Moving URI methods to cpp file

--- system/jlib/juri.hpp
+++ system/jlib/juri.hpp
@@ -105,69 +105,7 @@ class URI
     UriParserStateA state;
     UriUriA uri;
 
-    void populateFields()
-    {
-        // Scheme (defines which resolver to use, see above)
-        StringBuffer schemeStr(uri.scheme.afterLast - uri.scheme.first, uri.scheme.first);
-        schemeStr.toLowerCase();
-        if (strcmp(schemeStr.str(), "hpcc") == 0)
-            scheme = URIScheme_hpcc;
-        else if (strcmp(schemeStr.str(), "file") == 0)
-            scheme = URIScheme_file;
-        else
-            scheme = URIScheme_error;
-
-        // Server
-        server.user.set(uri.userInfo.first, uri.userInfo.afterLast - uri.userInfo.first);
-        server.host.set(uri.hostText.first, uri.hostText.afterLast - uri.hostText.first);
-        StringAttr portStr(uri.portText.first, uri.portText.afterLast - uri.portText.first);
-        server.port = atoi(portStr.get()); // More - use default ports?
-
-        // Path
-        UriPathSegmentA* cur = uri.pathHead;
-        StringBuffer pathStr;
-        if (uri.absolutePath || scheme == URIScheme_file)
-            pathStr.append("/");
-        bool first = true;
-        while (cur)
-        {
-            if (!first)
-                pathStr.append("/");
-            pathStr.append(cur->text.afterLast - cur->text.first, cur->text.first);
-            first = false;
-            cur = cur->next;
-        }
-        path.path.set(pathStr.str());
-
-        // Extra info
-        if (scheme == URIScheme_hpcc)
-        {
-            StringBuffer query(uri.query.afterLast - uri.query.first, uri.query.first);
-            query.toLowerCase();
-            if (strcmp(query.str(), "super") == 0)
-            {
-                path.type = URIFile_super;
-                path.subname.set(uri.fragment.first, uri.fragment.afterLast - uri.fragment.first);
-                path.index = 0;
-            }
-            else if (strcmp(query.str(), "stream") == 0)
-            {
-                path.type = URIFile_stream;
-                StringAttr index(uri.fragment.first, uri.fragment.afterLast - uri.fragment.first);
-                path.index = atoi(index.get());
-            }
-            else
-            {
-                path.type = URIFile_logic;
-                path.index = 0;
-            }
-        }
-        else
-        {
-            path.type = URIFile_local;
-            path.index = 0;
-        }
-    }
+    void populateFields();
 
 public:
     URI(const char* path)
@@ -213,6 +151,11 @@ public:
     {
         return &path;
     }
+
+    // MORE - is this the best way?
+    void appendSchemeStr(StringBuffer& buf);
+    void appendServerStr(StringBuffer& buf);
+    void appendPathStr(StringBuffer& buf);
 };
 
 #endif /* __JURI__ */
